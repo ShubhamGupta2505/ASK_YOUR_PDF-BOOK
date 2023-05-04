a17c0cbbe1ff4743b4dd4e5c3e856a9ef80555d1
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-05-04 12:26:57 +0530

    adding model

--- app.py
+++ app.py
@@ -5,6 +5,33 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import os
+
+def main():
+    load_dotenv()
+    # print(os.getenv('OPENAI_API_KEY'))
+    st.set_page_config(page_title="Ask Your Pdf")
+    st.header("Ask Your Pdf ")
+    pdf = st.file_uploader("Upload Your PDF ",type="pdf")
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+      st.write(text)
+
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+
+      chunk = text_splitter(pdf)
+      st.write(chunk)
+
+
 
 if __name__ == '__main__':
     main()
